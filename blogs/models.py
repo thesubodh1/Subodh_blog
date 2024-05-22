beb9edc4f4f4f3940817354a898c6255be2e3bd1
thesubodh1 <thesubodh1@gmail.com>
2024-05-22 21:46:21 +0545

    model created for database

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self) :
+        return f"{self.caption}"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name_plural = "Author"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    Author = models.ForeignKey(Author,on_delete=models.SET_NULL,null=True,related_name="post")
+    tag = models.ManyToManyField(Tag,null=False)
+
+    def __str__(self):
+        return f"{self.title} {self.excerpt} {self.content}"
+
+
